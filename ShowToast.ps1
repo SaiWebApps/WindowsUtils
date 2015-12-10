735d6ddb2ae72cd3684695d761243577d0a60b08
Sairam Krishnan <sakrish@microsoft.com>
2015-12-10 14:22:05 -0800

    Code for showing notifications

---
+++ ShowToast.ps1
@@ -0,0 +1,29 @@
+Param(
+	[Parameter(Mandatory=$True)][string]$Title,
+	[Parameter(Mandatory=$True)][string]$Text,
+
+	[int]$Duration,
+	[string]$IconSrc,
+	[switch]$SuccessIcon
+)
+
+# Default Toast duration = 10000 ms
+if (-Not $Duration) {
+	[int]$Duration = 10000
+}
+# If no icon is specified...
+if (-Not $IconSrc) {
+	[string]$IconSrc = "$pwd\ico\failure.ico"
+	if ($SuccessIcon) {
+		$IconSrc = "$pwd\ico\success.ico"
+	}
+}
+
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$Toast = New-Object System.Windows.Forms.NotifyIcon
+$Toast.BalloonTipTitle = $Title
+$Toast.BalloonTipText = $Text
+$Toast.Icon = $IconSrc
+$Toast.Visible = $True
+$Toast.ShowBalloonTip($Duration)
+$Toast.Dispose()
