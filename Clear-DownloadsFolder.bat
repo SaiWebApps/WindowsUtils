6c85fda7320597452c089271be3b91d7e04647c6
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-09-03 19:13:12 -0700

    Fixed bug in Clear-DownloadsFolder.bat
    Previously, it was deleting only the files in the root directory and all sub-directories.
    This commit added code to delete the folders as well.

--- Clear-DownloadsFolder.bat
+++ Clear-DownloadsFolder.bat
@@ -1,2 +1,9 @@
-path="%USERPROFILE%\Downloads\*.*"
-del /S /Q %path%
+set downloads_dir=%USERPROFILE%\Downloads\*
+set downloads_dir_contents=%downloads_dir%.*
+
+:: Delete all directories and subdirectories in Downloads, 
+:: along with the files that they contain.
+for /d %%i in (%downloads_dir%) do rd /S /Q %%i
+
+:: Delete all files in root (Downloads) directory.
+del /S /Q %downloads_dir_contents%
