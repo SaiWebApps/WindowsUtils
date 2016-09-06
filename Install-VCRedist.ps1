8f78d62cc6e2c28d25496329f4600c7d699c2303
Sairam Bharatwaj <Sairam@SAKRISHDELLXPS.redmond.corp.microsoft.com>
2016-09-06 15:18:05 -0700

    Smarter Install-VCRedist.ps1
    Checks system for installed VC++ redistributable versions and installs
    missing ones for you.

--- Install-VCRedist.ps1
+++ Install-VCRedist.ps1
@@ -1,8 +1,3 @@
-Param(
-    [string]$ExcludeVersions,
-    [string]$Delimiter = ','
-)
-
 # Run this script only if we're using an elevated PowerShell environment.
 $wid=[System.Security.Principal.WindowsIdentity]::GetCurrent()
 $prp=new-object System.Security.Principal.WindowsPrincipal($wid)
@@ -25,46 +20,68 @@ Start-BitsTransfer -Source $Url -Destination $DownloadZipTo
 
 # Run installers.
 [string]$AllPackagesFolder = $UnzipTo + '\Fix'
-# Delete the installers for the versions specified in ExcludeVersions.
-if ($ExcludeVersions) {
-    $ExcludeVersions.Split(',') | % {
-        Remove-Item -Recurse -Force $($AllPackagesFolder + '\' + $_)
+
+# Determine the versions that have already been installed on the system,
+# and delete the appropriate installers.
+[string[]]$InstalledVersions = 
+    Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | ? { 
+        $_.DisplayName -and $_.DisplayName.StartsWith('Microsoft Visual C++') 
+    } | % { $_.DisplayName }
+
+$InstalledVersions | ? { $_.Contains('x86') } | % { 
+    [string]$version = $_.Split(' ')[3]
+    [string]$installerPath = $($AllPackagesFolder + '\' + $version + '\vcredist_x86.exe')
+    if (Test-Path $installerPath) {
+        Remove-Item $installerPath
+    }
+}
+$InstalledVersions | ? { $_.Contains('x64') } | % { 
+    [string]$version = $_.Split(' ')[3]
+    [string]$installerPath = $($AllPackagesFolder + '\' + $version + '\vcredist_x64.exe')
+    if (Test-Path $installerPath) {
+        Remove-Item $installerPath
     }
 }
 
-dir $AllPackagesFolder | ? { $_.mode[0] -eq 'd' } | % {
+# First, isolate all directories in $AllPackagesFolder.
+# Then, isolate all directories with at least 1 installer.
+# Then, execute each installer in each directory.
+dir $AllPackagesFolder | ? { $_.mode[0] -eq 'd' } | ? { 
+    $(dir $($AllPackagesFolder + '\' + $_.Name) | measure).Count -gt 0
+} | % {
+    [string]$currentVersionDir = $($AllPackagesFolder + '\' + $_.Name)
     Push-Location
+    cd $currentVersionDir
 
-    cd $($AllPackagesFolder + '\' + $_.name)
-    switch ($_.name) 
+    # Execute installer. Pass appropriate flags based on VC++ version.
+    switch ($_.Name) 
     {
         2005 {
-            .\vcredist_x86.exe /q | Out-Null;
-            .\vcredist_x64.exe /q | Out-Null;
+            dir | % {
+                & ".\$_" /q | Out-Null
+            };
             break
-        } 
-        
+        }
+
         2008 {
-            .\vcredist_x86.exe /qb | Out-Null;
-            .\vcredist_x64.exe /qb | Out-Null;
+            dir | % {
+                & ".\$_" /qb | Out-Null
+            };
             break
         }
         
-        2010 {
-            .\vcredist_x86.exe /passive /norestart | Out-Null;
-            .\vcredist_x64.exe /passive /norestart | Out-Null;
-            break
-        }
-        
+        2010 {}
         2012 {
-            .\vcredist_x86.exe /passive /norestart | Out-Null;
-            .\vcredist_x64.exe /passive /norestart | Out-Null;
+            dir | % {
+                & ".\$_" /passive /norestart | Out-Null
+            };
             break
         }
         
         2013 {
-            .\vcredist_x86.exe /install /passive /norestart | Out-Null;
-            .\vcredist_x64.exe /install /passive /norestart | Out-Null;
+            dir | % {
+                & ".\$_" /install /passive /norestart | Out-Null
+            };
             break
         }
 
